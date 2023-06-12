c3e164481999bb47e637be47fa0d276fc8c5274d
AlexJamisone <business.homeit@gmail.com>
2023-06-12 19:11:52 +0300

    Add statictik

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,11 @@ import '../styles/global.css';
 const MyApp: AppType = ({ Component, pageProps }) => {
 	return (
 		<ChakraProvider theme={theme}>
+			<script
+				async
+				src="https://analytics.umami.is/script.js"
+				data-website-id="74de3feb-46bd-4b39-80bf-4cfb99bdc279"
+			></script>
 			<ClerkProvider localization={ruRU} {...pageProps}>
 				<Navigation />
 				<Component {...pageProps} />
