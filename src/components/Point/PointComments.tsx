7c023b7b2e6c563b354e44217df87506442e5fdd
AlexJamisone <business.homeit@gmail.com>
2023-06-26 23:09:21 +0300

    Add recapcha stuff

--- src/components/Point/PointComments.tsx
+++ src/components/Point/PointComments.tsx
@@ -15,10 +15,11 @@ import {
 import dayjs from 'dayjs';
 import 'dayjs/locale/ru';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useState } from 'react';
+import { createRef, useState } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 import { usePointContext } from '~/context/pointContext';
+import { env } from '~/env.mjs';
 import { api } from '~/utils/api';
-
 dayjs.locale('ru');
 
 const PointComments = () => {
@@ -38,32 +39,45 @@ const PointComments = () => {
 	const [value, setValue] = useState({
 		input: '',
 		id: point.id,
+		errorRecap: false,
 	});
 	const toast = useToast();
 	const ctx = api.useContext();
+	const recapthcaRef = createRef<ReCAPTCHA>();
 
 	const handlClick = () => {
-		create(
-			{
-				content: value.input,
-				pointId: value.id,
-			},
-			{
-				onSuccess: () => {
-					void ctx.comments.invalidate();
-					toast({
-						description: 'Комметарий успешно опубликован! ✔',
-						position: 'top-left',
-						status: 'success',
-						isClosable: true,
-					});
-					setValue({
-						...value,
-						input: '',
-					});
+		const token = recapthcaRef.current?.getValue();
+		if (token?.length === 0) {
+			setValue({
+				...value,
+				errorRecap: true,
+			});
+		} else {
+			create(
+				{
+					content: value.input,
+					pointId: value.id,
 				},
-			}
-		);
+				{
+					onSuccess: () => {
+						void ctx.comments.invalidate();
+						void recapthcaRef.current?.reset();
+						toast({
+							description: 'Комметарий успешно опубликован! ✔',
+							position: 'top-left',
+							status: 'success',
+							isClosable: true,
+						});
+						setValue({
+							...value,
+							input: '',
+							errorRecap: false,
+						});
+					},
+				}
+			);
+			console.log('create');
+		}
 	};
 	return (
 		<Stack w="100%">
@@ -74,7 +88,11 @@ const PointComments = () => {
 						value={value.input}
 						onChange={(e) => {
 							reset();
-							setValue({ ...value, input: e.target.value });
+							setValue({
+								...value,
+								input: e.target.value,
+								errorRecap: false,
+							});
 						}}
 						placeholder="Добавь свои комментарии"
 					/>
@@ -83,11 +101,48 @@ const PointComments = () => {
 					</FormErrorMessage>
 				</FormControl>
 			</Stack>
+			{value.input.length >= 1 && (
+				<Stack
+					as={motion.div}
+					initial={{ opacity: 0, y: -50 }}
+					animate={{
+						opacity: 1,
+						y: 0,
+						transition: {
+							type: 'spring',
+							duration: 0.5,
+							delay: 0.3,
+						},
+					}}
+					layout
+					border={(value.errorRecap && '1px solid') || undefined}
+					borderRadius="md"
+					borderColor="orange.300"
+					boxShadow="lg"
+					mx="47px"
+					alignItems="center"
+				>
+					<ReCAPTCHA
+						ref={recapthcaRef}
+						sitekey={env.NEXT_PUBLIC_RECAPCHA_KEY_SITE}
+						theme="light"
+						onChange={() =>
+							setValue({ ...value, errorRecap: false })
+						}
+					/>
+				</Stack>
+			)}
+			{value.errorRecap && (
+				<Text fontSize={13} textColor="orange.300" textAlign="center">
+					Подтвердите что вы человек
+				</Text>
+			)}
 			<Button
 				onClick={handlClick}
 				isLoading={isLoading}
 				isDisabled={isLoading}
 				type="button"
+				mt={3}
 			>
 				Оставить комметарий
 			</Button>
